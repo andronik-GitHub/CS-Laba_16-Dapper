34eeebaee70b2ca078887e69baa0ffed51a91a40
kazmi <costellationoblivion@gmail.com>
2022-12-11 23:49:31 +0200

    Добавьте файлы проекта.

---
+++ Dap/PART_TWO/CreateProcedures.cs
@@ -0,0 +1,14 @@
+﻿using Dapper;
+using Microsoft.Data;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+internal class CreateProcedures
+{
+    public static async Task CreateProcedures()
+    {
+
+    }
+}
